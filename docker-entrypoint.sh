a0e2f4727e1f529c854095dbfe36e118dd06bb76
bailangvvk <94160310+bailangvvk@users.noreply.github.com>
2025-06-23 17:34:15 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 NGINX_PORT="${NGINX_PORT:-80}"
 
 # 替换端口变量，生成最终虚拟主机配置
-envsubst '${NGINX_PORT}' < /nginx.vh.default.conf > /opt/openresty/nginx/conf/default.conf
+envsubst '${NGINX_PORT}' < /nginx.vh.default.conf > /opt/openresty/nginx/conf/nginx.conf
 
 exec "$@"
