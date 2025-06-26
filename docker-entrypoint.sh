9afe4c9ae1f03659b8e5a6f72ac1e24770a60d5a
bailangvvk <94160310+bailangvvk@users.noreply.github.com>
2025-06-26 21:13:51 +0800

    Delete docker-entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-NGINX_PORT="${NGINX_PORT:-80}"
-
-# 替换端口变量，生成最终虚拟主机配置
-envsubst '${NGINX_PORT}' < /nginx.vh.default.conf > /usr/local/openresty/nginx/conf/nginx.conf
-
-exec "$@"
